9345b6c637ee93a52969331c9aec1705f217b465
Maciej Kobus <maciej.kobus@ez.no>
2019-05-28 14:25:58 +0200

    EZP-30609: Renamed all *.yml files to *.yaml

--- src/bundle/BehatExtension.php
+++ src/bundle/BehatExtension.php
@@ -26,7 +26,7 @@ class BehatExtension implements Extension
             $container,
             new FileLocator(__DIR__ . '/Resources/config')
         );
-        $loader->load('extension.yml');
+        $loader->load('extension.yaml');
     }
 
     public function process(ContainerBuilder $container)
