2e25ecb6607f2dfb0db904a1fa8dea74e2f6b3fe
Maciej Kobus <maciej.kobus@ez.no>
2019-06-04 09:51:18 +0200

    Fixed TestSiteaccessCommand to use injected siteaccess

--- src/bundle/Command/TestSiteaccessCommand.php
+++ src/bundle/Command/TestSiteaccessCommand.php
@@ -6,21 +6,38 @@
  */
 namespace EzSystems\BehatBundle\Command;
 
-use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use eZ\Publish\Core\MVC\Symfony\SiteAccess;
 
-class TestSiteaccessCommand extends ContainerAwareCommand
+class TestSiteaccessCommand extends Command
 {
+    /** @var string|null */
+    protected static $defaultName = 'ez:behat:siteaccess';
+
+    /** @var \eZ\Publish\Core\MVC\Symfony\SiteAccess */
+    private $siteaccess;
+
+    /**
+     * @param \eZ\Publish\Core\MVC\Symfony\SiteAccess $siteaccess
+     */
+    public function __construct(SiteAccess $siteaccess)
+    {
+        $this->siteaccess = $siteaccess;
+
+        parent::__construct();
+    }
+
     protected function configure()
     {
         $this
-            ->setName('ez:behat:siteaccess')
+            ->setName(self::$defaultName)
             ->setDescription('Outputs the name of the active siteaccess');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln($this->getContainer()->get('ezpublish.siteaccess')->name);
+        $output->writeln($this->siteaccess->name);
     }
 }
