27dd116c4d047de8ed4fd73ab790c64c5c2b6bf6
Marek Nocon <marek.nocon@ez.no>
2019-06-18 09:21:15 +0200

    Add dragmock and Object classes

---
+++ src/lib/Browser/Object/Element.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+namespace EzSystems\Behat\Browser\Object;
+
+use EzSystems\Behat\Browser\Context\BrowserContext;
+
+/** Abstract for pages elements */
+abstract class Element
+{
+    /** @var int */
+    public $defaultTimeout = 5;
+
+    /* @var \EzSystems\Behat\Browser\Context\BrowserContext */
+    protected $context;
+
+    protected $fields;
+
+    public function __construct(BrowserContext $context)
+    {
+        $this->context = $context;
+    }
+
+    public function verifyVisibility(): void
+    {
+    }
+}
