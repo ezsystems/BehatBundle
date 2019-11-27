a0b122d4e8c588cee108dea9fc29fb4aaf8f631e
Marek Nocoń <marek.nocon@ez.no>
2019-11-27 09:36:15 +0100

    Added Twig PHPType extension

---
+++ src/bundle/Templating/Twig/PHPTypeExtension.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace EzSystems\BehatBundle\Templating\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class PHPTypeExtension extends AbstractExtension
+{
+    public function getFilters()
+    {
+        return [
+            new TwigFilter('php_type', function ($var) {
+                $type = gettype($var);
+
+                if ($type === 'object') {
+                    return get_class($var);
+                }
+
+                return $type;
+            }),
+        ];
+    }
+}
