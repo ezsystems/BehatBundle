7ae36736876603a1ff95191545237e374bce5030
Maciej Kobus <maciej.kobus@ez.no>
2020-03-14 19:01:17 +0100

    Symfony 5 compatibility

--- tests/Core/Log/LogFileReaderTest.php
+++ tests/Core/Log/LogFileReaderTest.php
@@ -22,7 +22,7 @@ class LogFileReaderTest extends TestCase
     /** @var LogFileReader */
     private $logReader;
 
-    public function setUp()
+    public function setUp(): void
     {
         $this->fileSystemRoot = vfsStream::setup();
         $this->logReader = new LogFileReader();
