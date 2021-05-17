83002f6499d6a32664bbb6324710b1b9b627b50a
Marek Nocoń <mnocon@users.noreply.github.com>
2021-05-17 17:11:55 +0200

    Added time measurements to TimeContext

--- src/lib/Core/Context/TimeContext.php
+++ src/lib/Core/Context/TimeContext.php
@@ -7,14 +7,55 @@
 namespace EzSystems\Behat\Core\Context;
 
 use Behat\Behat\Context\Context;
+use PHPUnit\Framework\Assert;
+use Symfony\Component\Stopwatch\Stopwatch;
 
 class TimeContext implements Context
 {
+    private $stopwatch;
+
+    public function __construct(Stopwatch $stopwatch)
+    {
+        $this->stopwatch = $stopwatch;
+    }
+
     /**
      * @Given I wait :number seconds
      */
     public function iWait($number): void
     {
         sleep($number);
+    }
+
+    /**
+     * @Given I start measuring time
+     */
+    public function iStartMeasuingTime(): void
+    {
+        $this->stopwatch->start('event');
+    }
+
+    /**
+     * @Given the action took no longer than :seconds seconds
+     */
+    public function actionTookNoLongerThan(string $maxDuration): void
+    {
+        $this->stopwatch->stop('event');
+        $actualDuration = $this->stopwatch->getEvent('event')->getDuration() / 1000;
+        $this->stopwatch->reset();
+
+        Assert::assertLessThanOrEqual($maxDuration, $actualDuration);
+    }
+
+    /**
+     * @Given the action took longer than :seconds seconds
+     */
+    public function actionTookLongerThan(string $maxDuration): void
+    {
+        $this->stopwatch->stop('event');
+        $actualDuration = $this->stopwatch->getEvent('event')->getDuration() / 1000;
+        $this->stopwatch->reset();
+
+        Assert::assertGreaterThan($maxDuration, $actualDuration);
     }
 }
