47bf5946633d9ac1891cab04506c3375b1e83fac
Marek Nocoń <mnocon@users.noreply.github.com>
2021-08-02 09:41:32 +0200

    Added ElementsCountGreaterThanCondition

---
+++ tests/Browser/Element/Condition/ElementsCountGreaterThanConditionTest.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace EzSystems\Behat\Test\Browser\Element\Condition;
+
+use EzSystems\Behat\Test\Browser\Element\BaseTestCase;
+use Ibexa\Behat\Browser\Element\Condition\ElementsCountGreaterThanCondition;
+use Ibexa\Behat\Browser\Locator\CSSLocator;
+use PHPUnit\Framework\Assert;
+
+class ElementsCountGreaterThanConditionTest extends BaseTestCase
+{
+    public function testExpectedCountPresent(): void
+    {
+        $searchedElementLocator = new CSSLocator('searched-id', 'searched-test');
+        $condition = new ElementsCountGreaterThanCondition(
+            $this->createElementWithChildElement('root', $searchedElementLocator, 'ChildText'),
+            $searchedElementLocator,
+            0
+        );
+
+        Assert::assertTrue($condition->isMet());
+    }
+
+    public function testExpectedCountNotPresent(): void
+    {
+        $searchedElementLocator = new CSSLocator('searched-id', 'searched-test');
+        $condition = new ElementsCountGreaterThanCondition(
+            $this->createElementWithChildElement('root', $searchedElementLocator, 'ChildText'),
+            $searchedElementLocator,
+            1
+        );
+        $invokingElement = $this->createElement('Test');
+        $invokingElement->method('getTimeout')->willReturn(5);
+
+        Assert::assertFalse($condition->isMet());
+        Assert::assertEquals(
+            "The found number of items (1) matching CSS locator 'searched-id': 'searched-test' was not greater than expected value (1). Timeout value: 5 seconds.",
+            $condition->getErrorMessage($invokingElement)
+        );
+    }
+}
