b2ca906dd403531d1671dfb1528f3df51660271a
Marek Nocoń <mnocon@users.noreply.github.com>
2021-08-04 09:30:24 +0200

    Added support for Commerce

--- src/bundle/Resources/encore/ez.config.manager.js
+++ src/bundle/Resources/encore/ez.config.manager.js
@@ -1,11 +1,15 @@
 const path = require('path');
 
 module.exports = (eZConfig, eZConfigManager) => {
+
+    const dragMockScriptPath = '../public/js/scripts/drag-mock.js';
+    const seleniumDebugStylePath = '../public/css/selenium-debug.css';
+
     eZConfigManager.add({
         eZConfig,
         entryName: 'ezplatform-admin-ui-layout-css',
         newItems: [
-            path.resolve(__dirname, '../public/css/debug.css')
+            path.resolve(__dirname, seleniumDebugStylePath)
         ]
     });
 
@@ -13,27 +17,38 @@ module.exports = (eZConfig, eZConfigManager) => {
         eZConfig,
         entryName: 'ezplatform-admin-ui-security-base-css',
         newItems: [
-            path.resolve(__dirname, '../public/css/debug.css')
+            path.resolve(__dirname, seleniumDebugStylePath)
         ]
     });
 
-    if (!eZConfig.entry['ezplatform-page-builder-edit-js'] || !eZConfig.entry['ezplatform-form-builder-common-js']) {
-        return;
+    if (eZConfig.entry['ezplatform-page-builder-edit-js']) {
+        const dragMockScriptPath = '../public/js/scripts/drag-mock.js';
+        eZConfigManager.add({
+            eZConfig,
+            entryName: 'ezplatform-page-builder-edit-js',
+            newItems: [
+                path.resolve(__dirname, dragMockScriptPath),
+            ],
+        });
     }
     
-    const dragMockScriptPath = '../public/js/scripts/drag-mock.js';
-    eZConfigManager.add({
-        eZConfig,
-        entryName: 'ezplatform-page-builder-edit-js',
-        newItems: [
-            path.resolve(__dirname, dragMockScriptPath),
-        ],
-    });
-    eZConfigManager.add({
-        eZConfig,
-        entryName: 'ezplatform-form-builder-common-js',
-        newItems: [
-            path.resolve(__dirname, dragMockScriptPath),
-        ],
-    });
+    if (eZConfig.entry['ezplatform-form-builder-common-js']) {
+        eZConfigManager.add({
+            eZConfig,
+            entryName: 'ezplatform-form-builder-common-js',
+            newItems: [
+                path.resolve(__dirname, dragMockScriptPath),
+            ],
+        });
+    }
+
+    if (eZConfig.entry['ezcommerce-shop-pagelayout-css']) {
+        eZConfigManager.add({
+            eZConfig,
+            entryName: 'ezcommerce-shop-pagelayout-css',
+            newItems: [
+                path.resolve(__dirname, seleniumDebugStylePath)
+            ]
+        });
+    }
 };
