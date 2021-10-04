725d44d5afe6b87afdad9c1e4e5cae6dd28c85dc
Marek Nocoń <mnocon@users.noreply.github.com>
2021-10-04 16:44:08 +0200

    Added env-file

--- bin/.travis/prepare_multirepository_setup.sh
+++ bin/.travis/prepare_multirepository_setup.sh
@@ -3,11 +3,11 @@
 cd "$HOME/build/project";
 
 # Drop database used by default connection
-docker-compose exec --user www-data app sh -c "php bin/console doctrine:database:drop --connection=default --force"
+docker-compose --env-file=.env exec --user www-data app sh -c "php bin/console doctrine:database:drop --connection=default --force"
 
 # Run setup
-docker-compose exec --user www-data app sh -c "vendor/bin/ezbehat --mode=standard --profile=setup --suite=multirepository -c=behat_ibexa_oss.yaml"
-docker-compose exec --user www-data app sh -c "composer run post-install-cmd"
+docker-compose --env-file=.env exec --user www-data app sh -c "vendor/bin/ezbehat --mode=standard --profile=setup --suite=multirepository -c=behat_ibexa_oss.yaml"
+docker-compose --env-file=.env exec --user www-data app sh -c "composer run post-install-cmd"
 
 # Reinstal database using the new repository
-docker-compose exec --user www-data app sh -c "php bin/console ibexa:install"
+docker-compose --env-file=.env exec --user www-data app sh -c "php bin/console ibexa:install"
