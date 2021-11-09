97852ca47c53ad71de6a07d16de80f6d912909c0
Marek Nocoń <mnocon@users.noreply.github.com>
2021-11-09 11:55:56 +0100

    Adapted multirepo setup to GA

--- bin/.travis/prepare_multirepository_setup.sh
+++ bin/.travis/prepare_multirepository_setup.sh
@@ -3,14 +3,14 @@
 cd "$HOME/build/project";
 
 # Drop database used by default connection
-docker-compose exec --user www-data app sh -c "php bin/console doctrine:database:drop --connection=default --force"
+docker-compose --env-file=.env exec -T --user www-data app sh -c "php bin/console doctrine:database:drop --connection=default --force"
 
 # Clear SPI cache
-docker-compose exec --user www-data app sh -c 'php bin/console cache:pool:clear ${CACHE_POOL:-cache.tagaware.filesystem}'
+docker-compose --env-file=.env exec -T --user www-data app sh -c 'php bin/console cache:pool:clear ${CACHE_POOL:-cache.tagaware.filesystem}'
 
 # Run setup
-docker-compose exec --user www-data app sh -c "vendor/bin/ezbehat --mode=standard --profile=setup --suite=multirepository -c=behat_ibexa_oss.yaml"
-docker-compose exec --user www-data app sh -c "composer run post-install-cmd"
+docker-compose --env-file=.env exec -T --user www-data app sh -c "vendor/bin/ezbehat --mode=standard --profile=setup --suite=multirepository -c=behat_ibexa_oss.yaml"
+docker-compose --env-file=.env exec -T --user www-data app sh -c "composer run post-install-cmd"
 
 # Reinstal database using the new repository
-docker-compose exec --user www-data app sh -c "php bin/console ibexa:install"
+docker-compose --env-file=.env exec -T --user www-data app sh -c "php bin/console ibexa:install"
