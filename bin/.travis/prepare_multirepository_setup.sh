7ce4694370f48f38d21794d9bb5ab50d9cce8bd4
Marek Nocoń <mnocon@users.noreply.github.com>
2021-11-19 17:39:42 +0100

    Move to Ga

--- bin/.travis/prepare_multirepository_setup.sh
+++ bin/.travis/prepare_multirepository_setup.sh
@@ -3,11 +3,11 @@
 cd "$HOME/build/ezplatform";
 
 # Drop database used by default connection
-docker-compose exec --user www-data app sh -c "php bin/console doctrine:database:drop --connection=default --force"
+docker-compose exec -T --user www-data app sh -c "php bin/console doctrine:database:drop --connection=default --force"
 
 # Run setup
-docker-compose exec --user www-data app sh -c "bin/ezbehat --mode=standard --profile=setup --suite=multirepository"
-docker-compose exec --user www-data app sh -c "composer run post-install-cmd"
+docker-compose exec -T --user www-data app sh -c "bin/ezbehat --mode=standard --profile=setup --suite=multirepository"
+docker-compose exec -T --user www-data app sh -c "composer run post-install-cmd"
 
 # Reinstal database using the new repository
-docker-compose exec --user www-data app sh -c "composer ezplatform-install"
+docker-compose exec -T --user www-data app sh -c "composer ezplatform-install"
