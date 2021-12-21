16734d137d4eb80c75c39e1813715d0499921805
Marek Nocoń <mnocon@users.noreply.github.com>
2021-12-21 10:34:43 +0100

    Use non-deprecated entry names

--- src/bundle/Resources/encore/ez.config.manager.js
+++ src/bundle/Resources/encore/ez.config.manager.js
@@ -14,13 +14,13 @@ module.exports = (eZConfig, eZConfigManager) => {
     const dragMockScriptPath = path.resolve(__dirname, '../public/js/scripts/drag-mock.js');
     const transitionListenerScriptPath = path.resolve(__dirname, '../public/js/scripts/transition-listener.js');
     const scriptsMap = {
-        'ezcommerce-shop-pagelayout-css': [seleniumDebugStylePath],
-        'ezplatform-admin-ui-content-type-edit-js': [dragMockScriptPath],
-        'ezplatform-admin-ui-layout-css': [seleniumDebugStylePath],
-        'ezplatform-admin-ui-layout-js': [transitionListenerScriptPath],
-        'ezplatform-admin-ui-security-base-css': [seleniumDebugStylePath],
-        'ezplatform-form-builder-common-js': [dragMockScriptPath],
-        'ezplatform-page-builder-edit-js': [dragMockScriptPath],
+        'ibexa-commerce-shop-pagelayout-css': [seleniumDebugStylePath],
+        'ibexa-admin-ui-content-type-edit-js': [dragMockScriptPath],
+        'ibexa-admin-ui-layout-css': [seleniumDebugStylePath],
+        'ibexa-admin-ui-layout-js': [transitionListenerScriptPath],
+        'ibexa-admin-ui-security-base-css': [seleniumDebugStylePath],
+        'ibexa-form-builder-common-js': [dragMockScriptPath],
+        'ibexa-page-builder-edit-js': [dragMockScriptPath],
     };
 
     Object.entries(scriptsMap).forEach(addEntry);
