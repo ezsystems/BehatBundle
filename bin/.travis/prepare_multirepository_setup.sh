f6639852e5b562076bbf6c5ad54761ed593668c8
Marek Nocoń <mnocon@users.noreply.github.com>
2022-03-17 11:45:28 +0100

    Rebranded Allure reports and adapted to Github Actions

--- bin/.travis/prepare_multirepository_setup.sh
+++ bin/.travis/prepare_multirepository_setup.sh
@@ -9,7 +9,7 @@ docker-compose exec -T --user www-data app sh -c "php bin/console doctrine:datab
 docker-compose exec -T --user www-data app sh -c 'php bin/console cache:pool:clear ${CACHE_POOL:-cache.tagaware.filesystem}'
 
 # Run setup
-docker-compose exec -T --user www-data app sh -c "vendor/bin/ezbehat --mode=standard --profile=setup --suite=multirepository -c=behat_ibexa_oss.yaml"
+docker-compose exec -T --user www-data app sh -c "vendor/bin/ibexabehat --mode=standard --profile=setup --suite=multirepository -c=behat_ibexa_oss.yaml"
 docker-compose exec -T --user www-data app sh -c "composer run post-install-cmd"
 
 # Reinstal database using the new repository
