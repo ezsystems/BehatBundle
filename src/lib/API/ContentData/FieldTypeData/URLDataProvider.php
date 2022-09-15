923fb1a03feea8c9fca938c20f023784778246c2
Marek Nocoń <mnocon@users.noreply.github.com>
2022-09-15 12:11:20 +0200

    Added support for specyfing URL text

--- src/lib/API/ContentData/FieldTypeData/URLDataProvider.php
+++ src/lib/API/ContentData/FieldTypeData/URLDataProvider.php
@@ -8,6 +8,8 @@ declare(strict_types=1);
 
 namespace EzSystems\Behat\API\ContentData\FieldTypeData;
 
+use eZ\Publish\Core\FieldType\Url\Value;
+
 class URLDataProvider extends AbstractFieldTypeDataProvider
 {
     public function supports(string $fieldTypeIdentifier): bool
@@ -17,6 +19,15 @@ class URLDataProvider extends AbstractFieldTypeDataProvider
 
     public function generateData(string $contentTypeIdentifier, string $fieldIdentifier, string $language = 'eng-GB')
     {
-        return $this->getFaker()->url;
+        return new Value($this->getFaker()->url, $this->getFaker()->realText(80, 1));
+    }
+
+    public function parseFromString(string $value)
+    {
+        $values = explode(',', $value);
+        $url = $values[0];
+        $text = $values[1] ?? null;
+
+        return new Value($url, $text);
     }
 }
